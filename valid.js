e5d6774d19e5f83a4820c7de1cb8c88da1d5c768
shane <sdrafahl@iastate.edu>
2017-09-25 21:18:04 -0500

    finished the first part

--- valid.js
+++ valid.js
@@ -1,5 +1,22 @@
 function validate() {
     var fname = document.getElementById('fname').value
-     var letterNumber = /^[0-9a-zA-Z]+$/;
+    var lname = document.getElementById('lname').value
+    var country = document.getElementById('country').value
+    var gender = document.getElementById('gender').value
+    var regex = /^[a-z]+$/i;
+    if(regex.test(fname)) {
+        document.getElementById('fnameImg').src="correct.png"
+    } else {
+        document.getElementById('fnameImg').src="wrong.png"
+    }
+    
+    if(regex.test(lname)) {
+        document.getElementById('lnameImg').src="correct.png"
+    } else {
+        document.getElementById('lnameImg').src="wrong.png"
+    }
+
+    document.getElementById('genderImg').src="correct.png"
+    document.getElementById('stateImg').src="correct.png"
 
 }
